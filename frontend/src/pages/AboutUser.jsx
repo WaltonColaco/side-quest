97d2b0cfbb3b67984393237b50935e2bfb15c69f
donna mathew <donnamathew280@gmail.com>
2026-02-21 12:57:47 -0700

    merge conflicts fixed

--- frontend/src/pages/AboutUser.jsx
+++ frontend/src/pages/AboutUser.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Link } from "react-router-dom";
 import logo from "../../hacked-logo.png";
 import userImage from "../../user.png.png";
@@ -23,35 +22,14 @@ function AboutUser() {
         </div>
 
         <div className="login-right">
-          <img className="login-user-image" src={userImage} alt="User profile" />
+          <img
+            className="login-user-image"
+            src={userImage}
+            alt="User profile"
+          />
         </div>
       </div>
       <div className="login-bottom-shape" aria-hidden="true" />
-=======
-import logo from "../../hacked-logo.png";
-import avatarImg from "../assets/profile-avatar.svg";
-
-const options = ["Resident/Visitor", "Design Professional", "Business Owner"];
-
-function AboutUser() {
-  return (
-    <section className="about-user-screen full no-card" aria-label="About user">
-      <div className="about-user-content about-user-content--full">
-        <div className="about-user-left">
-          <h2>Who are you? I am a..</h2>
-          <div className="about-user-buttons">
-            {options.map((opt) => (
-              <button key={opt} className="about-user-btn" type="button">
-                {opt}
-              </button>
-            ))}
-          </div>
-        </div>
-        <div className="about-user-right">
-          <img src={avatarImg} alt="Profile avatar" className="about-user-avatar-img" />
-        </div>
-      </div>
->>>>>>> eb8c5f23cc4cb4c096778272d65ea067584f8ca2
     </section>
   );
 }
